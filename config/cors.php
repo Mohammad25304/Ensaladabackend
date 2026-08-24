8cd2889b9ef1f231f1cee84cb36250224900bcdc
Mohammad Sabbah <164613968+Mohammad25304@users.noreply.github.com>
2026-08-24 21:52:38 +0300

    adding the correct url of frontend

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://ensalad-frontend.vercel.app/'], // your Vite dev server's actual port
+    'allowed_origins' => ['https://ensalad-frontend-cfylnxe8d-mohammad-sabbahs-projects.vercel.app'], // your Vite dev server's actual port
 
     'allowed_origins_patterns' => [],
 
